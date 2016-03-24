e01a48fdbb52e3d9fcf982ec54adacb65422fe10
gcanti <giulio.canti@gmail.com>
2016-03-24 12:41:29 +0100

    rename config.json to index.json

--- test/createExpressPlayerFromConfig.js
+++ test/createExpressPlayerFromConfig.js
@@ -1,10 +1,10 @@
 var createExpressPlayerFromConfig = require('../lib/createExpressPlayerFromConfig');
-var config = require('./fixtures/config.json');
+var config = require('./fixtures/index.json');
 var app = createExpressPlayerFromConfig(config);
 
 // app.player.setId('/orders');
 
-var PORT = 5002;
+var PORT = 5000;
 app.listen(PORT, function () {
   console.log('express server listening on http://localhost:%s/', PORT); // eslint-disable-line no-console
 });
